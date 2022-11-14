8c4fef9ab915ac87587114f3cb0877ca28f69ff9
WoliverAraujo <109636440+WoliverAraujo@users.noreply.github.com>
2022-11-14 17:41:37 -0300

    Variaveis

---
+++ JS/index.js
@@ -0,0 +1,11 @@
+let nome = 'Carlos';/*Eu posso declarar e inicar uma variavel*/
+/*let nome;  Ou posso declarar uma variavel
+nome = 'Pedro' E depois iniciar está variável
+Mas não posso declar a mesma variavel duas vezes, sendo assim, so posso usar o let uma vez para declar a variavel*/
+console.log(nome, 'nasceu em 1989');
+console.log('Em 2018', nome, 'conheceu Maria');
+console.log(nome, 'casou com Maria em 2018');
+nome = 'Pedro' //A variavl, pode trocar de valor durante o programa
+console.log('Maria teve 1 filha com', nome, 'em 2019');
+console.log('A filha de', nome, 'se chama Amora');
+//Letras maiúsculas e minúsculas são diferentes
--- aula3/index.js
+++
@@ -1,1 +0,0 @@
-console.log('Meu nome é "Woliver". Estou apredendo JavaScript às', 4, 'da tarde');
