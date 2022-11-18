5bfcdce674df47353c7cfec9e496b2333c168f9d
WoliverAraujo <109636440+WoliverAraujo@users.noreply.github.com>
2022-11-18 11:48:31 -0300

    26.Numero

--- JS/index.js
+++ JS/index.js
@@ -1,12 +1,11 @@
-//               01234567
-let umaString = 'Um texto'
-console.log(umaString[3])//Retorna com um valor do texto. Neste caso o valor vai ser "t" porque a contagem começa do 0
-console.log(umaString.charAt(3))//Faz mesma coisa que o de cima
-console.log(umaString.indexOf('texto')) //Procura um texto ou letra e me fala em qual posição esta.
-console.log(umaString.lastIndexOf('texto')) // Igual o de cima. Porem começa de tras pra frente.
-console.log(umaString.lastIndexOf('t, 5'))// igual o de cima, mas neste caso está começando da posição 2.
-console.log(umaString.search(/m/)) //Pesquisa dentro da String
-console.log(umaString.replace(/Um/, 'Outro texto de novo'))// .replace procura a palavra na string e substitui pelo o que você escolher
-console.log(umaString.concat(' Outro texto'))
-console.log(umaString + ' Outro texto')
-console.log(`${umaString} Outro texto`)
+let num1 = 1;
+let num2 = 2.5;
+let num3 = 1.58556454684;
+
+
+console.log(num1.toString() + num2)
+console.log(num3.toFixed(2))//arredonda somente na visualização
+num3 = Number(num3.toFixed(2))//arredonda dentro do programa
+console.log(Number.isInteger(num1))// para saber so o numero é inteiro
+console.log(Number.isInteger(num3))
+console.log(Number.isInteger(num2))
