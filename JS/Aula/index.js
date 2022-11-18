2960ea66b0a06d7cabc0ce8e3767477605e16457
WoliverAraujo <109636440+WoliverAraujo@users.noreply.github.com>
2022-11-18 15:54:39 -0300

    31. Função

--- JS/Aula/index.js
+++ JS/Aula/index.js
@@ -1,23 +1,8 @@
-//              0        1        2   
-const alunos = ['Luiz', 'Maria', 'João']; //cria array com lista de nomes.
-//console.log(alunos[1]);
+function saudacao(nome) {
+console.log(`Bom dia ${nome}!`)
+return 12345;
+}
 
-//alunos [0] = 'Pedro';
-//alunos [3] = 'Woliver';
-//alunos[alunos.length] = 'Luiza';
-//alunos[alunos.length] = 'Ana';
-//alunos[alunos.length] = 'Henrique';
-
-alunos.unshift('Luiza') //adiciona no inicio
-alunos.push('Ana') // adiciona no fim
-alunos.push('Henrique')
-
-const removidoFim = alunos.pop(); // pop apaga o ultimo item da lista. Criei uma variavel para armazenar o item exluido.
-const removidoInicio = alunos.shift(); // shift apaga o primeiro item da lista. Criei uma variavel para armazenar o item exluido.
-
-//delete alunos[2] // delete, apaga indice da lista
-
-console.log(removidoFim)
-console.log(removidoInicio)
-console.log(alunos.slice(0,3)) // lembrando que o ultimo numero não conta. Neste caso vai o numeor 0,1,2
-console.log(alunos)
+saudacao(`Woliver`);
+const varialvel = saudacao(`Maria`);
+console.log(varialvel)
