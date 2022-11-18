25aea67bbbd189301607168f6c8d3e69bce2dc87
WoliverAraujo <109636440+WoliverAraujo@users.noreply.github.com>
2022-11-18 15:33:12 -0300

    29 Arrays Basico

--- JS/Aula/index.js
+++ JS/Aula/index.js
@@ -1,8 +1,23 @@
-/*let num1 = 9.54578;
-let num2 = Math.floor(num1); //Arredonda para baixo
-let num3 = Math.ceil(num1); // Areedonda para cima
-let num4 = Math.round(num1); // Arredonda para o numero inteiro mais proximo
-console.log(num2, num3, num4);*/
+//              0        1        2   
+const alunos = ['Luiz', 'Maria', 'João']; //cria array com lista de nomes.
+//console.log(alunos[1]);
 
-let aleatorio = Math.round(Math.random()*(10-5)+5); // Math.rando gera numero aleatório. Math round define a margem em que este numero vai ser gerado
-console.log(aleatorio)
+//alunos [0] = 'Pedro';
+//alunos [3] = 'Woliver';
+//alunos[alunos.length] = 'Luiza';
+//alunos[alunos.length] = 'Ana';
+//alunos[alunos.length] = 'Henrique';
+
+alunos.unshift('Luiza') //adiciona no inicio
+alunos.push('Ana') // adiciona no fim
+alunos.push('Henrique')
+
+const removidoFim = alunos.pop(); // pop apaga o ultimo item da lista. Criei uma variavel para armazenar o item exluido.
+const removidoInicio = alunos.shift(); // shift apaga o primeiro item da lista. Criei uma variavel para armazenar o item exluido.
+
+//delete alunos[2] // delete, apaga indice da lista
+
+console.log(removidoFim)
+console.log(removidoInicio)
+console.log(alunos.slice(0,3)) // lembrando que o ultimo numero não conta. Neste caso vai o numeor 0,1,2
+console.log(alunos)
