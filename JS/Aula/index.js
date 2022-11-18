04ad073c05304d97a44eba5017bd8a840973dd10
WoliverAraujo <109636440+WoliverAraujo@users.noreply.github.com>
2022-11-18 16:16:22 -0300

    31. Função 2

--- JS/Aula/index.js
+++ JS/Aula/index.js
@@ -1,8 +1,15 @@
-function saudacao(nome) {
-console.log(`Bom dia ${nome}!`)
-return 12345;
-}
+//function soma(x ,y){
+//   const resultado = x + y;
+//   return resultado;// nada inserido depois de return e dentro do {} vai ser compatado.
+//}
 
-saudacao(`Woliver`);
-const varialvel = saudacao(`Maria`);
-console.log(varialvel)
+//console.log(soma(2,2)); // A função vai tanto fazer a soma
+//console.log(soma(`Woliver` ,`Araujo`)); // Como tambem vai fazer a concatenação.
+
+const raiz = (n) => n**0.5;//{ //=> se chama elo funcion e tem a mesma finalidade da funcion e elemina o return
+    //return n ** 0.5; // Calculo da raiz quadrada.
+//};
+
+console.log(raiz(9));
+console.log(raiz(16));
+console.log(raiz(25));
