e6714e029b76479dbd631e249807279b6fb52007
WoliverAraujo <109636440+WoliverAraujo@users.noreply.github.com>
2022-11-18 18:04:39 -0300

    32. Objetos 1

--- JS/Aula/index.js
+++ JS/Aula/index.js
@@ -1,15 +1,9 @@
-//function soma(x ,y){
-//   const resultado = x + y;
-//   return resultado;// nada inserido depois de return e dentro do {} vai ser compatado.
-//}
+function criaPessoa (nome, sobrenome, idade) {
+    return {
+        nome: nome,
+        sobrenome: sobrenome,
+        idade: idade
+    };
+}
 
-//console.log(soma(2,2)); // A função vai tanto fazer a soma
-//console.log(soma(`Woliver` ,`Araujo`)); // Como tambem vai fazer a concatenação.
-
-const raiz = (n) => n**0.5;//{ //=> se chama elo funcion e tem a mesma finalidade da funcion e elemina o return
-    //return n ** 0.5; // Calculo da raiz quadrada.
-//};
-
-console.log(raiz(9));
-console.log(raiz(16));
-console.log(raiz(25));
+const pessoa1 = criaPessoa('Woliver', 'Araujo','33')
