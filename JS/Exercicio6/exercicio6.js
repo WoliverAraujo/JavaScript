2c910b2a51431b19be0afe9272a04aa23ae8f805
WoliverAraujo <109636440+WoliverAraujo@users.noreply.github.com>
2022-11-19 01:33:31 -0300

    34. Exercicio6

--- JS/Exercicio6/exercicio6.js
+++ JS/Exercicio6/exercicio6.js
@@ -1,8 +1,33 @@
 function meuFormulario () {
     const form = document.querySelector('.form');
-    form.onsubmit = function (evento) {
+    const resultado= document.querySelector('resultado')
+    const pessoas = [];
+//    form.onsubmit = function (evento) {
+//        evento.preventDefault(); //preventDefault() é para prevenir o que acontece por padrão. Neste Caso, //não enviar o formulario
+//        alert(1)
+//    };
+    /*let contador = 1;*/
+    function recebeEventoForm (evento) {
+        evento.preventDefault();
+       /*console.log (`Fomr não enviado ${contador}`)
+        contador++;*/
+        const nome = form.querySelector('nome');
+        const sobrenome = form.querySelector('sobrenome');
+        const peso = form.querySelector('peso');
+        const altura = form.querySelector('altura');
+
+        pessoas.push ({
+            nome: nome.value,
+            sobrenome: sobrenome.value,
+            peso: peso.value,
+            altura: altura.value,
+        })
+
+        console.log(pessoas);
+        resultado.innerHTML += <p>${nome.value} ${sobrenome.value} #{peso.value} ${altura}</p>
+
         
-        alert(1)
-    };
+    }
+    form.addEventListener('submit',recebeEventoForm); //Basicamente está me perguntando qual evento eu quero "escutar". Neste caso eu coloquei submit dentro dos parenteses para referir e este evento
 }
 meuFormulario();
