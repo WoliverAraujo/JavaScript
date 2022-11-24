a6ac894e1c923ddd7ada03eda3eb6059d2949577
WoliverAraujo <109636440+WoliverAraujo@users.noreply.github.com>
2022-11-24 10:02:44 -0300

    Calculadora de IMC

--- JS/assets/js/main.js
+++ JS/assets/js/main.js
@@ -2,15 +2,31 @@ const form = document.querySelector('#formulario');
 
 form.addEventListener('submit', function (e) {
     e.preventDefault();
-    console.log('Evento interrompido');
-    setResultado('Olá Mundo!')
+    const inputPeso = e.target.querySelector('#peso');
+    const inputAltura = e.target.querySelector('#altura');
+
+    const peso = Number(inputPeso.value);
+    const altura = Number(inputAltura.value);
+
+    if (!peso){
+        setResultado('Peso inválido', false);
+        return;
+    }
+    if (!altura){
+        setResultado('Altura inválida', false);
+        return;
+    }
 });
 
-function setResultado (msg){
+function criaP () {
+    const p = document.createElement('p');
+    return p;
+}
+
+function setResultado (msg, isValid) {
     const resultado = document.querySelector('#resultado');
     resultado.innerHTML = '';
-    const p = document.createElement('p');
-    p.classList.add('paragrafo-resultado')
-    p.innerHTML = 'Qual coisa';
+    const p = criaP();
+    p.innerHTML = msg;
     resultado.appendChild(p);
 }
