99a757f54d0496310c15114839b8c8d7ce827ee2
Woliver <woliveraraujo@hotmail.com>
2022-12-08 17:59:04 -0300

    Exercício 8

--- JS/Exercicio7/assets/js/main.js
+++ JS/Exercicio7/assets/js/main.js
@@ -1,1 +1,11 @@
-const hello = name => {}
+const paragrafos = document.querySelector('.paragrafos');
+const ps = paragrafos.querySelectorAll('p');
+
+const estilosBody = getComputedStyle(document.body);
+const backgrondColorBody = estilosBody.backgroundColor;
+console.log (backgrondColorBody);
+
+for (let p of ps){
+    p.style.backgroundColor = backgrondColorBody;
+    p.style.color = '#ffffff'
+}
