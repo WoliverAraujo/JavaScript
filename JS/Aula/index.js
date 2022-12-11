e832ad4327b77022e66fe6a884b9b3d8553e00b3
Woliver <woliveraraujo@hotmail.com>
2022-12-11 15:29:08 -0300

    Break e Continue

--- JS/Aula/index.js
+++ JS/Aula/index.js
@@ -1,19 +1,17 @@
-function random (min, max) {
-    const r = Math.random() * (max - min) + min;
-    return Math.floor(r);
+const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+for ( let i in numeros) {
+    let numero = numeros[i];
+
+    if (numero ===2){
+        console.log ('Pulei o numero 2')
+        continue;
+    }
+
+    if (numero === 7){
+        console.log('O número 7 foi encontrado aqui... então é o fim')
+        break;
+    }
+
+    console.log(numero);
 }
-
-const min = 1;
-const max = 50;
-let rand = 10;
-
-
-while (rand !== 10) {
-    rand = random(min, max);
-    console.log(rand);
-}
-
-do {
-    rand =random(min, max);
-    console.log(rand);
-} while (rand!== 10);
